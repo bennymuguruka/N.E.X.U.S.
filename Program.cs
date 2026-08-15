e0a5f777e9b9ed934d43158886b7772ff96f788f
Benny Muguruka <bennymuguruka21@gmail.com>
2026-08-15 22:08:43 +0200

    Fixing Structure

--- Program.cs
+++ Program.cs
@@ -1,15 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace N.E.X.U.S_Warehouse_and_Logistics_Hub
+namespace PRG_281_Project
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Warehouse warehouse = new Warehouse();
+
+            warehouse.StartMonitoring();
+
+
+            bool running = true;
+
+
+            while (running)
+            {
+                Console.Clear();
+
+                Console.WriteLine(
+                    "================================");
+
+                Console.WriteLine(
+                    "       N.E.X.U.S.");
+
+                Console.WriteLine(
+                    " Warehouse & Logistics System");
+
+                Console.WriteLine(
+                    "================================");
+
+
+                Console.WriteLine(
+                    "\n1. View Inventory");
+
+                Console.WriteLine(
+                    "2. Create Order");
+
+                Console.WriteLine(
+                    "3. Process Order");
+
+                Console.WriteLine(
+                    "4. View Orders");
+
+                Console.WriteLine(
+                    "5. Exit");
+
+
+                Console.Write(
+                    "\nChoose: ");
+
+                string choice =
+                    Console.ReadLine();
+
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case "1":
+
+                            warehouse.ShowItems();
+
+                            break;
+
+
+                        case "2":
+
+                            warehouse.CreateOrder();
+
+                            break;
+
+
+                        case "3":
+
+                            warehouse.ProcessOrders();
+
+                            break;
+
+
+                        case "4":
+
+                            foreach (Order order
+                                     in warehouse.Orders)
+                            {
+                                Console.WriteLine(
+                                    $"Order #{order.Id} - " +
+                                    $"{order.Status} - " +
+                                    $"Vehicle: " +
+                                    $"{order.VehicleId}");
+                            }
+
+                            break;
+
+
+                        case "5":
+
+                            running = false;
+
+                            break;
+
+
+                        default:
+
+                            Console.WriteLine(
+                                "Invalid option.");
+
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(
+                        $"ERROR: {ex.Message}");
+                }
+
+
+                if (running)
+                {
+                    Console.WriteLine(
+                        "\nPress ENTER...");
+
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
